b8a98f4ea6038caee7450cd96dc49977f977ef4d
julius1409 <Julius.Quentell@kracht.com>
2026-01-20 10:21:40 +0100

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,14 @@
+(() => {
+  const topbar = document.querySelector(".topbar");
+  if (!topbar) return;
+
+  const update = () => {
+    const y = window.scrollY || 0;
+    if (y <= 2) topbar.classList.remove("is-hidden");
+    else topbar.classList.add("is-hidden");
+  };
+
+  window.addEventListener("scroll", update, { passive: true });
+  update();
+})();
 
