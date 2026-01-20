0c619a16b55b9183922b6a2db957098e96fcfed5
julius1409 <Julius.Quentell@kracht.com>
2026-01-20 10:18:04 +0100

    Remove topbar scroll visibility toggle

--- js/ui.js
+++ js/ui.js
@@ -1,13 +1,1 @@
-(() => {
-  const topbar = document.querySelector(".topbar");
-  if (!topbar) return;
 
-  const onScroll = () => {
-    const y = window.scrollY || 0;
-    if (y <= 2) topbar.classList.remove("is-hidden");
-    else topbar.classList.add("is-hidden");
-  };
-
-  window.addEventListener("scroll", onScroll, { passive: true });
-  onScroll();
-})();
